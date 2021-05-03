61b06d94e636c94285d089be1b1f622e3619c655
Satinder Sharma <80735720+satinder-saavi@users.noreply.github.com>
2021-05-03 11:43:10 +0530

    added readme and updated settings.py

--- TwilioProject/settings.py
+++ TwilioProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'TwilioProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'static_cdn'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
